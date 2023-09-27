2e73b5ade9b231f5c9ebeb0fc5d17a24d37bc9be
asarantos <alexandros@dragontale.net>
2023-09-27 17:41:10 +0300

    implemented login (back and front) big progression on sign up nav bar ui implemented jwt done https done

---
+++ Server/Server/Model/DatabaseSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Model;
+
+public class DatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+}
---
+++ Server/Server/Services/MongoService.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using Model;
+using MongoDB.Bson.Serialization.Conventions;
+using SharedLibrary;
+
+namespace Server.Services;
+
+public abstract class MongoService<T> where  T : UniqueClass
+{
+    public readonly IMongoCollection<T> _collection;
+
+    public MongoService(IOptions<DatabaseSettings> databaseSettings, string collectionName)
+    {
+        var mongoClient = new MongoClient(
+            databaseSettings.Value.ConnectionString);
+        
+        var conventionPack = new ConventionPack { new IgnoreExtraElementsConvention(true) };
+        ConventionRegistry.Register("IgnoreExtraElements", conventionPack, type => true);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            databaseSettings.Value.DatabaseName);
+
+        _collection = mongoDatabase.GetCollection<T>(
+            collectionName);
+    }
+
+    public async Task<List<T>> GetAsync() =>
+        await _collection.Find(_ => true).ToListAsync();
+
+    public async Task<T?> GetAsync(string uid) =>
+        await _collection.Find(x => x.uid == uid).FirstOrDefaultAsync();
+
+    public async Task CreateAsync(T newUserProfile) =>
+        await _collection.InsertOneAsync(newUserProfile);
+
+    public async Task UpdateAsync(string uid, T updatedUserProfile) =>
+        await _collection.ReplaceOneAsync(x => x.uid == uid, updatedUserProfile);
+
+    public async Task RemoveAsync(string uid) =>
+        await _collection.DeleteOneAsync(x => x.uid == uid);
+}
